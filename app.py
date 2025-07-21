e5f501c7950649099a7ed8931337ac503726a4e9
Celine Lin <yuling.lin010@gmail.com>
2025-07-21 17:21:20 +0800

    update app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import openai
 from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
 from utils.vision_parser import parse_floorplan_image
 from utils.prompt_templates import generate_narrative_prompt, call_gpt_narrative
 from utils.docx_generator import generate_docx
@@ -11,6 +12,7 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
+CORS(app, origins=["https://www.atophouse.com"])
 UPLOAD_FOLDER = "static"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
